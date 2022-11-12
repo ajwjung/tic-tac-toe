b2eb3c6767ba7698003ad83734a0c8e40412b487
Amy Jung <ajwjung@outlook.com>
2022-11-11 16:10:24 -0800

    feat: Display gameboard on webpage

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,26 @@
 const Player = (name, marker) => {
-    return {
-        name,
-        marker
-    }
+    return { name, marker }
 }
+
+// A module for the board
+const GameBoard = (() => {
+    let board = ["X", "O", "X", "O", "X", "O", "X", "O", "O"];
+    const gridCells = document.querySelectorAll(".cell");
+
+    // Display board contents
+    const render = () => {
+        for (let i = 0; i < 9; i++) {
+            gridCells[i].innerHTML = board[i];
+        }
+    }
+
+    return { board, render };
+})();
 
 // Main module to run the game
 const Game = (function() {
     const playerOne = Player("Bob", "X");
     const playerTwo = Player("Jack", "O");
-
-    // A module for the board
-    const GameBoard = (() => {
-        let board = ["X", "O", "X", "O", "X", "O", "X", "O", "O"];
-    })();
-
+    
+    GameBoard.render();
 })();
