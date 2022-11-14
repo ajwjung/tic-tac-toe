1b73a107c0ab8301af5ee273a43c756d5ba1dfc4
Amy Jung <ajwjung@outlook.com>
2022-11-14 13:50:49 -0800

    feat: Update array and place marker on click

--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,58 @@ const Player = (name, marker) => {
 
 // A module for the board
 const GameBoard = (() => {
+    // Temporary contents
     let board = ["X", "O", "X", "O", "X", "O", "X", "O", "O"];
+    
+    return { board };
+})();
+
+// A module for displaying things
+const DisplayController = (() => {
     const gridCells = document.querySelectorAll(".cell");
 
     // Display board contents
     const render = () => {
         for (let i = 0; i < 9; i++) {
-            gridCells[i].innerHTML = board[i];
+            gridCells[i].innerHTML = GameBoard.board[i];
         }
-    }
+    };
 
-    return { board, render };
+    const getPlayerTurn = (counter) => {
+        // playerOne starts the game
+        if (counter % 2 === 0) return "playerOne"
+        else return "playerTwo";
+    };
+
+    const placeMarker = (p1, p2, counter) => {
+        gridCells.forEach(cell => {
+            cell.addEventListener("click", function (e) {
+                // get cell ID to use as array index
+                const index = e.target.getAttribute("id").slice(-1);
+                const playerTurn = getPlayerTurn(counter);
+                
+                // update board array to add new marker
+                if (playerTurn === "playerOne") {
+                    GameBoard.board[index] = p1.marker;
+                } else if (playerTurn === "playerTwo") {
+                    GameBoard.board[index] = p2.marker;
+                }
+
+                render();
+                counter++;
+            })
+        })
+    };
+
+    return { render, placeMarker }
 })();
 
 // Main module to run the game
-const Game = (function() {
+const Game = (() => {
     const playerOne = Player("Bob", "X");
     const playerTwo = Player("Jack", "O");
+    let turnsCounter = 0; 
     
-    GameBoard.render();
+    DisplayController.render();
+    DisplayController.placeMarker(playerOne, playerTwo, turnsCounter);
 })();
